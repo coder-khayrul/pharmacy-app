908aad45ec26ba298abe43b14de747cd260aa03c
shakiburrahmanmahim4-alt <shakiburrahmanmahim4@gmail.com>
2026-09-02 13:18:21 +0600

    fix: Solve Loading Error

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,20 @@
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const hideSplash = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    hideSplash();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
